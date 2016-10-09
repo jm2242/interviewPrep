da54535e8c8929c13a3a69e8d85192de6d5afa03
Jonathan Mares <jmares93@gmail.com>
2016-10-09 16:52:35 -0400

    followed tutorial on implementing BFS, Dijkstras, and astar

--- graphs.py
+++ graphs.py
@@ -3,6 +3,8 @@ import heapq
 from implementation import *
 
 # Requires Python 3
+# this is not my code
+# I have taken this from Red Blob Games -> amazing resource
 
 # basic queue wrapper for deque
 class Queue:
@@ -32,7 +34,41 @@ class PriorityQueue:
 	def get(self):
 		return heapq.heappop(self.elements)[1] # get data of tuple
  
+def dijkstra_search(graph, start, goal):
+	frontier = PriorityQueue()
+	frontier.put(start, 0)
+	came_from = {}
+	cost_so_far = {}
+	came_from[start] = None
+	cost_so_far[start] = 0
 
+	while not frontier.empty():
+		current = frontier.get()
+
+		if current == goal:
+			break
+
+		for next in graph.neighbors(current):
+			new_cost = cost_so_far[current] + graph.cost(current, next)
+			if next not in cost_so_far or new_cost < cost_so_far[next]:
+				cost_so_far[next] = new_cost
+				priority = new_cost
+				frontier.put(next, priority)
+				came_from[next] = current		
+
+
+	return came_from, cost_so_far
+
+def reconstruct_path(came_from, start, goal):
+	current = goal
+	path = [current]
+	while current != start:
+		current = came_from[current]
+		path.append(current)
+
+	path.append(start)
+	path.reverse()
+	return path
 
 
 
@@ -138,6 +174,43 @@ def breadth_first_search_3(graph, start, goal):
 
 	return came_from
 
+def heuristic(a,b):
+	(x1, y1) = a
+	(x2, y2) = b
+	return abs(x1 - x2) + abs(y1 - y2)
+
+
+def a_star_search_j(graph, start, goal):
+	frontier = PriorityQueue()
+	frontier.put(start, 0)
+	came_from = {}
+	cost_so_far = {}
+	came_from[start] = None
+	cost_so_far[start] = 0
+
+	while not frontier.empty():
+		current = frontier.get()
+
+		if current == goal:
+			break
+
+		for next in graph.neighbors(current):
+			new_cost = cost_so_far[current] + graph.cost(current, next)
+
+			# if next hasn't been visited or the cost to visit this node is now lower
+			if next not in cost_so_far or new_cost < cost_so_far[next]:
+				cost_so_far[next] = new_cost
+
+				# choose a priority 
+				priority = new_cost + heuristic(goal, next)
+				frontier.put(next, priority)
+				came_from[next] = current
+
+	return came_from, cost_so_far
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -159,9 +232,17 @@ if __name__ == '__main__':
 	#parents = breadth_first_search_2(g, (8,7))
 	parents = breadth_first_search_3(g, (8,7), (17,2))
 
-	draw_grid(g, width=2, point_to=parents, start=(8,7), goal=(17,2))
-
-
+	#draw_grid(g, width=2, point_to=parents, start=(8,7), goal=(17,2))
+	# came_from, cost_so_far = dijkstra_search(diagram4, (1,4),(7,8))
+	# draw_grid(diagram4, width=3, point_to=came_from, start=(1, 4), goal=(7, 8))
+	# print()
+	# draw_grid(diagram4, width=3, number=cost_so_far, start=(1, 4), goal=(7, 8))
+	# print()
+	# draw_grid(diagram4, width=3, path=reconstruct_path(came_from, start=(1, 4), goal=(7, 8)))
+	came_from, cost_so_far = a_star_search_j(diagram4, (1, 4), (7, 8))
+	draw_grid(diagram4, width=3, point_to=came_from, start=(1, 4), goal=(7, 8))
+	print()
+	draw_grid(diagram4, width=3, number=cost_so_far, start=(1, 4), goal=(7, 8))
 
 
 
